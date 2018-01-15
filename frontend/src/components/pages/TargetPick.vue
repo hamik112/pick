c9d1b5690c01f48ec186ab1b441ed55415e6fa8d
pc-HP\pc <joys1006>
2018-01-15 14:51:33 +0900

    타겟 2 팝업 삭제

--- frontend/src/components/pages/TargetPick.vue
+++ frontend/src/components/pages/TargetPick.vue
@@ -64,7 +64,6 @@
 	// 팝업
 	import TargetChartPop from '@/components/popup/Target_chart'
 	import TargetMake1 from '@/components/popup/Target_make_01'
-	import TargetMake2 from '@/components/popup/Target_make_02'
 	// UI
 	import Select from '@/components/ui/Select'
 	import Calendar from '@/components/ui/Calendar'
@@ -75,7 +74,6 @@
 		components: {
 			'TargetChartPop': TargetChartPop,
 			'TargetMake1': TargetMake1,
-			'TargetMake2': TargetMake2,
 			'ui-select': Select,
 			'ui-calendar':Calendar
 		},
