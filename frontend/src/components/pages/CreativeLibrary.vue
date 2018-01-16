33dcdacf1ead5e6929fce5c77b93a10a3c9e97be
hotdog <hotdog@digginglab.io>
2018-01-16 18:45:53 +0900

    fix cr iframe height

--- frontend/src/components/pages/CreativeLibrary.vue
+++ frontend/src/components/pages/CreativeLibrary.vue
@@ -15,35 +15,38 @@
 </template>
 
 <script>
-	//레이아웃 호출
-	export default {
-	  name: 'CreativeLibrary',
-
-	  data () {
-	    return {
-	    	iframe: {
-		      src: 'http://www.pickdata.co.kr:7777/creative/',
-		      height:0,
-		      loaded: false
-		    }
-	    }
-	  },
-	  methods:{
-	  	load: function(el){
-	    	this.iframe.loaded = true;
-	    	this.iframe.height = window.innerHeight
-	    	console.log(this.getDocHeight(el))
-	    },
-	    getDocHeight(doc) {
-		    doc = doc || document;
-		    let body = doc.body
-		    let html = doc.documentElement
-		    let height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight )
-		    return height;
+//레이아웃 호출
+export default {
+	name: 'CreativeLibrary',
+	mounted () {
+		let me = this
+		window.addEventListener("message", receiveMessage, false);
+		function receiveMessage(e) {
+			me.iframe.height = e.data
 		}
-	  }
+	},
+
+	data () {
+		return {
+			iframe: {
+				src: 'http://www.pickdata.co.kr:7777/creative/',
+				height:0,
+				loaded: false
+			}
+		}
+	},
+	methods:{
+		load: function(el){
+			this.iframe.loaded = true;
+			this.iframe.height = window.innerHeight
+			setTimeout(function() {
+				document.getElementById(el).style = 'display: block'
+			}, 1000)
+		}
 	}
+}
 </script>
 
 <style lang="css" scoped>
+#creative-frame { display: none; }
 </style>
