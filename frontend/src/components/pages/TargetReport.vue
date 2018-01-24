425605342b12e5560e145af2029ddef459944056
pc-HP\pc <joys1006>
2018-01-24 19:06:04 +0900

    타겟 리포트 툴팁 닫기버튼 버그 수정

--- frontend/src/components/pages/TargetReport.vue
+++ frontend/src/components/pages/TargetReport.vue
@@ -448,15 +448,15 @@ export default {
 			const tools = document.getElementsByClassName('interest_view')
 			const subId = event.target.className
 			const subEl = document.getElementById(subId)
+			for(let i = 0; i < tools.length; i++) {
+				tools[i].style.display = "none"
+			}
 			if (subEl !== null) {
-				for(let i = 0; i < tools.length; i++) {
-					tools[i].style = "display:none"
-				}
 				if(index != 'close') {
 					if(subEl.style.display == 'block') {
-						subEl.style = "display:none"
+						subEl.style.display = "none"
 					}else{
-						subEl.style = "display:block"
+						subEl.style.display = "block"
 					}
 				}
 			}
