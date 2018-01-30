cc43df38ac8905381d2ba85e78af14bc585524b9
hotdog <hotdog@digginglab.io>
2018-01-30 12:31:02 +0900

    fix conversion front backend create&modify

--- backend/utils/facebookapis/targeting/targeting_conversion.py
+++ backend/utils/facebookapis/targeting/targeting_conversion.py
@@ -52,6 +52,54 @@ def create_conversion_customers(account_id, name, pixel_id, retention_days=30, p
         msg['error'] = e._error
         raise Exception(msg)
 
+def update_conversion_customers(custom_audience_id, name, pixel_id, retention_days=30, prefill=True,
+                              conversion_event_name="ViewContent"):
+    try:
+        custom_audience = CustomAudience(custom_audience_id)
+
+        rule = {
+            "inclusions": {
+                "operator": "or",
+                "rules": [
+                    {
+                        "event_sources": [
+                            {
+                                "type": "pixel",
+                                "id": pixel_id
+                            }
+                        ],
+                        "retention_seconds": retention_days * 24 * 60 * 60,
+                        "filter": {
+                            "operator": "and",
+                            "filters": [
+                                {
+                                    "field": "event",
+                                    "operator": "eq",
+                                    "value": conversion_event_name
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+
+        params = {}
+        params[CustomAudience.Field.name] = name
+        params[CustomAudience.Field.prefill] = prefill
+        params[CustomAudience.Field.rule] = rule
+
+        audience = custom_audience.remote_update(params=params)
+
+        return audience
+
+    except FacebookRequestError as e:
+        print(e)
+        msg = {}
+        msg['request_context'] = e._request_context
+        msg['error'] = e._error
+        raise Exception(msg)
+
 
 
 # 전환 URL완료 고객
@@ -109,8 +157,56 @@ def create_conversion_url_customers(account_id, name, pixel_id, retention_days=3
         msg['error'] = e._error
         raise Exception(msg)
 
+def update_conversion_url_customers(custom_audience_id, name, pixel_id, retention_days=30, prefill=True,
+                              current_url='', next_url=''):
+    try:
+        custom_audience = CustomAudience(custom_audience_id)
 
+        rule = {
+            "inclusions":{
+                "operator":"or",
+                "rules":[
+                    {
+                        "event_sources":[
+                            {
+                                "type":"pixel",
+                                "id":pixel_id
+                            }
+                        ],
+                        "retention_seconds":retention_days * 24 * 60 * 60,
+                        "filter":{
+                            "operator":"and",
+                            "filters":[
+                                {
+                                    "field":"url",
+                                    "operator":"i_contains",
+                                    "value":current_url
+                                },
+                                {
+                                    "field": "url",
+                                    "operator": "i_contains",
+                                    "value": next_url
+                                }
+                            ]
+                        },
+                        "template":"VISITORS_BY_URL"
+                    }
+                ]
+            }
+        }
 
+        params = {}
+        params[CustomAudience.Field.name] = name
+        params[CustomAudience.Field.prefill] = prefill
+        params[CustomAudience.Field.rule] = rule
 
+        audience = custom_audience.remote_update(params=params)
 
+        return audience
 
+    except FacebookRequestError as e:
+        print(e)
+        msg = {}
+        msg['request_context'] = e._request_context
+        msg['error'] = e._error
+        raise Exception(msg)
