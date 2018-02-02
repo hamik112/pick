f89e96f33b0049c1e9f6bc279489fe38880c49b3
pc-HP\pc <joys1006>
2018-02-02 15:39:53 +0900

    중복 UTM값 얼럿창 이후에도 적용되는 버그 수정

--- frontend/src/components/popup/target/UtmTarget.vue
+++ frontend/src/components/popup/target/UtmTarget.vue
@@ -345,14 +345,14 @@ export default {
         this.dialogOpen('수집 기간은 최대 180일까지만 가능합니다.', 'alert')
         this.collectionPeriod = 180
       } else if (this.collectionPeriod === '0') {
-        alert('수집 기간은 최소 1일입니다.')
+        this.dialogOpen('수집 기간은 최소 1일입니다.', 'alert')
         this.collectionPeriod = 1
       }
     },
 
     unvisitedPeriod (day) {
       if(day >= this.collectionPeriod) {
-        alert('미방문 기간은 수집 기간보다 작아야합니다.')
+        this.dialogOpen('미방문 기간은 수집 기간보다 작아야합니다.', 'alert')
         this.unvisitedPeriod = this.collectionPeriod - 1
       }
     },
@@ -505,7 +505,6 @@ export default {
         if (gData[i].name === utmName) {
           //컨펌,얼럿 텍스트 - 메세지창 타입(confirm,alert) - 독립적모드이름(alert 메세지시 사용 X)
           this.dialogOpen('같은 UTM값이 존재합니다.', 'alert')
-          break
           this.inputUtmName = ''
           return false
         }
