e0455c99ac00388fb27f252ba5246c094b55d3ef
superman <joys1006>
2018-02-08 16:00:28 +0900

    타겟리포트 페이징 계산 올림처리추가

--- frontend/src/components/pages/TargetReport.vue
+++ frontend/src/components/pages/TargetReport.vue
@@ -404,7 +404,7 @@
 																		<ul>
 																			<li>{{ numberFormat(item.inline_link_clicks) }}</li>
 																			<li>{{ item.inline_link_click_ctr }}</li>
-																			<li>￦{{ numberFormat(item.inline_link_click_cpc) }}</li>
+																			<li>￦{{ item.inline_link_click_cpc }}</li>
 																		</ul>
 																	</dd>
 																</dl>
@@ -445,14 +445,14 @@
 																	<dt></dt>
 																	<dd>
 																		<ul>
-																			<li class="line-16">{{ numberFormat(item.pickdata_custom_conv_total) }}</li>
+																			<li class="line-16">{{ item.pickdata_custom_conv_total }}</li>
 																			<!-- cost_per_action_type?-->
 																			<li class="line-16">{{ item.pickdata_custom_conv_total_cost }}</li>
-																			<li class="line-16">{{ numberFormat(item.pickdata_custom_conv_first) }}</li>
-																			<li class="line-16">{{ numberFormat(item.pickdata_custom_conv_second) }}</li>
-																			<li class="line-16">{{ numberFormat(item.pickdata_custom_conv_third) }}</li>
-																			<li class="line-16">{{ numberFormat(item.pickdata_custom_conv_fourth) }}</li>
-																			<li class="line-16">{{ numberFormat(item.pickdata_custom_conv_fifth) }}</li>
+																			<li class="line-16">{{ item.pickdata_custom_conv_first }}</li>
+																			<li class="line-16">{{ item.pickdata_custom_conv_second }}</li>
+																			<li class="line-16">{{ item.pickdata_custom_conv_third }}</li>
+																			<li class="line-16">{{ item.pickdata_custom_conv_fourth }}</li>
+																			<li class="line-16">{{ item.pickdata_custom_conv_fifth }}</li>
 																		</ul>
 																	</dd>
 																</dl>
@@ -799,7 +799,7 @@ export default {
 			var limit = 25
 			var page_total = this.pageTotal
 			var page_number = page_total / limit
-			this.pageRange.pageNumber = Math.round(page_number)
+			this.pageRange.pageNumber = Math.ceil(page_number)
 		},
 		clickPage (n) {
 			if(this.currentPage !== n) {
