146ef853c7c15eee4ffd139955ba6d59e3d38c8d
Jimin Park <parkjiminy@gmail.com>
2018-02-08 14:32:37 +0900

    fix apis

--- backend/utils/facebookapis/targeting/targeting_conversion.py
+++ backend/utils/facebookapis/targeting/targeting_conversion.py
@@ -52,6 +52,7 @@ def create_conversion_customers(account_id, name, pixel_id, retention_days=30, p
         msg['error'] = e._error
         raise Exception(msg)
 
+
 def update_conversion_customers(custom_audience_id, name, pixel_id, retention_days=30, prefill=True,
                               conversion_event_name="ViewContent"):
     try:
@@ -100,7 +101,155 @@ def update_conversion_customers(custom_audience_id, name, pixel_id, retention_da
         msg['error'] = e._error
         raise Exception(msg)
 
+# 미 전환 고객
+def create_non_conversion_customers(account_id, name, pixel_id, retention_days=30, prefill=True,
+                                    conversion_event_name="ViewContent"):
+    try:
+        ad_account = AdAccount(fbid=account_id)
 
+        rule = {
+            "inclusions": {
+                "operator": "and",
+                "rules": [
+                    {
+                        "event_sources": [
+                            {
+                                "type": "pixel",
+                                "id": pixel_id
+                            }
+                        ],
+                        # "retention_seconds": retention_days * 24 * 60 * 60,
+                        "retention_seconds": retention_days * 24 * 60 * 60,
+                        "filter": {
+                            "operator": "and",
+                            "filters": [
+                                {
+                                    "field": "url",
+                                    "operator": "i_contains",
+                                    "value": ""
+                                }
+                            ]
+                        },
+                        "template": "ALL_VISITORS"
+                    }
+                ]
+            },
+            "exclusions": {
+                "operator": "or",
+                "rules": [
+                    {
+                        "event_sources": [
+                            {
+                                "type": "pixel",
+                                "id": pixel_id
+                            }
+                        ],
+                        "retention_seconds": retention_days * 24 * 60 * 60,
+                        "filter": {
+                            "operator": "and",
+                            "filters": [
+                                {
+                                    "field": "event",
+                                    "operator": "eq",
+                                    "value": conversion_event_name
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+
+        params = {}
+        params[CustomAudience.Field.name] = name
+        params[CustomAudience.Field.prefill] = prefill
+        params[CustomAudience.Field.rule] = rule
+
+        audience = ad_account.create_custom_audience(params=params)
+
+        return audience
+
+    except FacebookRequestError as e:
+        print(e)
+        msg = {}
+        msg['request_context'] = e._request_context
+        msg['error'] = e._error
+        raise Exception(msg)
+
+# 미 전환 고객
+def update_non_conversion_customers(custom_audience_id, name, pixel_id, retention_days=30, prefill=True,
+                                    conversion_event_name="ViewContent"):
+    try:
+        custom_audience = CustomAudience(custom_audience_id)
+
+        rule = {
+            "inclusions": {
+                "operator": "and",
+                "rules": [
+                    {
+                        "event_sources": [
+                            {
+                                "type": "pixel",
+                                "id": pixel_id
+                            }
+                        ],
+                        # "retention_seconds": retention_days * 24 * 60 * 60,
+                        "retention_seconds": retention_days * 24 * 60 * 60,
+                        "filter": {
+                            "operator": "and",
+                            "filters": [
+                                {
+                                    "field": "url",
+                                    "operator": "i_contains",
+                                    "value": ""
+                                }
+                            ]
+                        },
+                        "template": "ALL_VISITORS"
+                    }
+                ]
+            },
+            "exclusions": {
+                "operator": "or",
+                "rules": [
+                    {
+                        "event_sources": [
+                            {
+                                "type": "pixel",
+                                "id": pixel_id
+                            }
+                        ],
+                        "retention_seconds": retention_days * 24 * 60 * 60,
+                        "filter": {
+                            "operator": "and",
+                            "filters": [
+                                {
+                                    "field": "event",
+                                    "operator": "eq",
+                                    "value": conversion_event_name
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+
+        params = {}
+        params[CustomAudience.Field.name] = name
+        params[CustomAudience.Field.prefill] = prefill
+        params[CustomAudience.Field.rule] = rule
+
+        audience = custom_audience.remote_update(params=params)
+
+        return audience
+
+    except FacebookRequestError as e:
+        print(e)
+        msg = {}
+        msg['request_context'] = e._request_context
+        msg['error'] = e._error
+        raise Exception(msg)
 
 # 전환 URL완료 고객
 def create_conversion_url_customers(account_id, name, pixel_id, retention_days=30, prefill=True,
